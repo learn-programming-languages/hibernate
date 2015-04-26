5ab3e8154d31071b4ca3ea33d847b2e3de6d5983
learn-programming-languages <timofiy.s.bilyi@gmail.com>
2015-04-26 11:53:24 +0300

    add annotations for property

--- src/main/java/ua/tim/hibernate/dto/UserDetails.java
+++ src/main/java/ua/tim/hibernate/dto/UserDetails.java
@@ -9,13 +9,13 @@ import javax.persistence.Id;
  */
 @Entity (name = "USER_DETAILS")
 public class UserDetails {
-
-    private int userId;
-
-
-    private String userName;
     @Id
     @Column(name = "USER_ID")
+    private int userId;
+
+    @Column(name="USER_NAME")
+    private String userName;
+
     public int getUserId() {
         return userId;
     }
@@ -23,9 +23,9 @@ public class UserDetails {
     public void setUserId(int userId) {
         this.userId = userId;
     }
-    @Column(name="USER_NAME")
+
     public String getUserName() {
-        return userName + "from getter";
+        return userName;
     }
 
     public void setUserName(String userName) {
