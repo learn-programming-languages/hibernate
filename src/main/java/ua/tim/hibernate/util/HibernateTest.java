1f2ff8e11a9bedfc487670c3e83a7340016fa2fd
learn-programming-languages <timofiy.s.bilyi@gmail.com>
2015-04-28 11:31:16 +0300

    Detachet Object variant #1

--- src/main/java/ua/tim/hibernate/util/HibernateTest.java
+++ src/main/java/ua/tim/hibernate/util/HibernateTest.java
@@ -19,19 +19,23 @@ public class HibernateTest {
             SessionFactory sessionFactory =  new Configuration().configure().buildSessionFactory();
             Session session = sessionFactory.openSession();
             session.beginTransaction();
-//            for (int i = 0 ;i < 10; i++) {
-//                UserDetails userDetails = new UserDetails();
-//                userDetails.setUserName("User #" + i);
-//                session.save(userDetails);
-//            }
+            UserDetails user = new UserDetails();
+            session.save(user);
             UserDetails userDetails = (UserDetails)session.get(UserDetails.class, 1);
-//            System.out.println(userDetails.getUserName());
-            userDetails.setUserName("UPDATE");
-            session.update(userDetails);
 
-            session.delete(userDetails);
             session.getTransaction().commit();
             session.close();
+
+            userDetails.setUserName("DETACHED OBJECT");
+
+            session = sessionFactory.openSession();
+            session.beginTransaction();
+
+            session.update(userDetails);
+
+            session.getTransaction().commit();
+            session.close();
+
         }
         catch (Throwable ex) {
             // Make sure you log the exception, as it might be swallowed
