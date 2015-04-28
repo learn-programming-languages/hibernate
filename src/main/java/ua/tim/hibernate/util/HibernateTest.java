27d9aa11e3d0933f9c24e40c743b446c861583e1
learn-programming-languages <timofiy.s.bilyi@gmail.com>
2015-04-28 13:11:53 +0300

    HQL and Query object

--- src/main/java/ua/tim/hibernate/util/HibernateTest.java
+++ src/main/java/ua/tim/hibernate/util/HibernateTest.java
@@ -1,9 +1,12 @@
 package ua.tim.hibernate.util;
 
+import org.hibernate.Query;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.cfg.Configuration;
 import ua.tim.hibernate.dto.UserDetails;
+
+import java.util.List;
 
 /**
  * Created by timofiybilyi on 4/26/15.
@@ -19,26 +22,37 @@ public class HibernateTest {
             SessionFactory sessionFactory =  new Configuration().configure().buildSessionFactory();
             Session session = sessionFactory.openSession();
             session.beginTransaction();
-            UserDetails user = new UserDetails();
-            user.setUserName("NEW OBJECT (PERSIST)");
-            session.save(user);
 
-            UserDetails userDetails = (UserDetails)session.get(UserDetails.class, 1);
+            for (int i = 0; i < 10; i++){
+                UserDetails user = new UserDetails();
+                user.setUserName("user details name: " + i);
+                session.save(user);
+            }
 
             session.getTransaction().commit();
             session.close();
 
-            userDetails.setUserName("DETACHED OBJECT");
+
 
             session = sessionFactory.openSession();
             session.beginTransaction();
 
-            //commit for see result
-            userDetails.setUserName("DETACHED OBJECT USE SELECT BEFORE UPDATE");
-            session.update(userDetails);
+            //create query object from class Userdetails
+            //Query query = session.createQuery("from UserDetails ");
+
+            //create query object from class Userdetails where property userId > 5
+            Query query = session.createQuery("from UserDetails where userId > 5");
+
+            //get all records from query (list)
+            List<UserDetails> listOfRowFromUserDetails = query.list();
+
 
             session.getTransaction().commit();
             session.close();
+
+            for (UserDetails item: listOfRowFromUserDetails){
+                System.out.println(item.getUserName());
+            }
 
         }
         catch (Throwable ex) {
